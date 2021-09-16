1df2fba5a9eb366661ab046a8d93ffeac4afb674
Borislav-source <tsv.borislav@gmail.com>
2021-09-16 20:51:00 +0300

    '

---
+++ 1.) Syntax, Functions and Statements/Exercises/Exercises/6.Road_radar.js
@@ -0,0 +1,32 @@
+function radar(speed, location) {
+    let speed_limit;
+    let difference;
+    let status;
+    switch (location) {
+        case 'motorway': speed_limit = 130; break;
+        case 'interstate': speed_limit = 90; break;
+        case 'city': speed_limit = 50; break;
+        case 'residential': speed_limit = 20; break;
+    }
+    if (speed > speed_limit) {
+        difference = speed - speed_limit;
+        if (difference <= 20) {
+            status = 'speeding';
+        }
+        else if (difference <= 40) {
+            status = 'exessive speeding';
+        }
+        else {
+            status = 'reckless driving';
+        }
+        console.log(`The speed is ${difference} km/h faster than the allowed speed of ${speed_limit} - ${status}`);
+    }
+    else {
+        console.log(`Driving ${speed} km/h in a ${speed_limit} zone`);
+    }
+}
+
+radar(200, 'motorway');
+radar(120, 'interstate');
+radar(21, 'residential');
+radar(40, 'city');
