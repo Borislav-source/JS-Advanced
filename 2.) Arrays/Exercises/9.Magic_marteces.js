b14b7dc87e2a95453c2c62ced0936cf20d4bcb65
Borislav-source <tsv.borislav@gmail.com>
2021-09-21 20:06:16 +0300

    Exercises

--- 2.) Arrays/Exercises/9.Magic_marteces.js
+++ 2.) Arrays/Exercises/9.Magic_marteces.js
@@ -1,10 +1,18 @@
 function magic(givenMatrix) {
+    let condition = 'true';
     first_row_sum = givenMatrix[0].reduce((a, b) =>a+b,0);
     for (let row of givenMatrix) {
         if (first_row_sum !== row.reduce((i, k)=> i+k)) {
-            console.log('false'); return;
+            condition = 'false'; 
         }
     }
+    var colSum = givenMatrix.reduce((a, b) => a.map((x, i) => x + b[i]));
+    colSum.forEach(sum => {
+        if (sum !== first_row_sum){
+            condition = 'false';
+        }   
+    })
+    console.log(condition);
 }
 
-magic([[4, 5, 5], [6, 5, 4], [5, 5, 5]])
+magic([[4, 6, 5], [6, 5, 4], [5, 5, 5]])
